b618777b1b209c42841efb85805e10b57f2fd62a
Siddharth Talla <109227989+Tallasiddharth@users.noreply.github.com>
2022-07-13 20:35:35 +0530

    Create List1.py

---
+++ List1.py
@@ -0,0 +1,56 @@
+
+
+
+@author: Sidd
+"""
+list1 = [1, "hello", "world", True]
+
+list2 = list(range(10))
+
+print(list1)
+
+print(list2)
+
+print(list1[0])
+
+print(list1[2:])
+
+print(list1[-3])
+
+print(list1[2])
+
+list = [1,2,3,4,5,6,7,8]
+
+list = {1,2,3,4,5,6}
+
+list = ["djfbfb",'ghfgfhf',"hfgfgdfgh"]
+
+a=[1,2,3,4,5,6,7,8,9]
+print(a[:2])
+
+a=[1,2,3,4,5,6,7,8,9]
+print(a[::4])
+
+a=[1,2,3,4,5,6,7,8,9]
+a[::2]
+
+
+a=[1,2,3,4,5,6,7,8,9]
+a[::2]=10,20,30,40,50,60
+print(a)
+
+a=[1,2,3,4,5]
+print(a[3:0:-1])
+
+def f(value, values):
+    v = 1
+    values[0] = 44
+t = 3
+v = [1, 2, 3]
+f(t, v)
+print(t, v[0])
+
+import random
+fruit_list = ['apple', 'banana', 'papaya', 'cherry']
+random.shuffle(fruit_list)
+print(fruit_list)
